a9661d5d69ae4cb3f9e57495954e97741f44a2b8
mvune <mvune01@gmail.com>
2019-05-26 23:58:50 +0200

    Dashboard home

--- resources/js/mijn-ee/containers/nav.js
+++ resources/js/mijn-ee/containers/nav.js
@@ -9,11 +9,13 @@ export default {
       name: 'Dashboard',
       url: '/mijn-ee/dashboard',
       icon: 'icon-pie-chart',
+      description: 'Uw financiën overzichtelijk verwerkt in hapklare grafiekjes en tabelletjes.',
     },
     {
       name: 'Transacties',
       url: '/mijn-ee/transacties',
       icon: 'icon-docs',
+      description: 'Upload transacties en bekijk het overzicht van uw transacties.',
       children: [
         {
           name: 'Overzicht',
@@ -31,11 +33,13 @@ export default {
       name: 'Categorieën',
       url: '/mijn-ee/categorieen',
       icon: 'icon-tag',
+      description: 'Maak categorieën aan om uw transacties gemakkelijk te sorteren.',
     },
     {
       name: 'Rekeningen',
       url: '/mijn-ee/rekeningen',
       icon: 'icon-star',
+      description: 'Bekijk uw rekeningen.',
     },
   ]
 }
--- resources/js/mijn-ee/views/home/Home.vue
+++ resources/js/mijn-ee/views/home/Home.vue
@@ -1,11 +1,76 @@
 <template>
   <div class="animated fadeIn">
-    
+    <div class="items-container">
+      <template v-for="item in navItems">
+        <div v-if="item.name !== 'Home'" :key="item.name" class="item">
+          <router-link :to="item.url">
+            <b-card>
+              <div class="mb-2">
+                <i :class="item.icon" class="icons gold"></i>
+              </div>
+
+              <h3 class="gold">{{ item.name }}</h3>
+
+              <b-card-text class="mt-4">
+                {{ item.description }}
+              </b-card-text>
+            </b-card>
+          </router-link>
+        </div>
+      </template>
+    </div>
   </div>
 </template>
 
+<style lang="scss" scoped>
+@import '../../variables.scss';
+
+.items-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.item {
+  position: relative;
+  width: 15rem;
+  margin-right: 0.5rem;
+  margin-bottom: 0.5rem;
+}
+
+.item:after {
+  content: "";
+  display: block;
+  padding-bottom: 100%;
+}
+
+.card {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  color: black;
+}
+
+.icons {
+  font-weight: bold;
+  font-size: 3rem;
+}
+
+.gold {
+  color: $theme-quaternary;
+}
+</style>
+
 <script>
+import nav from '../../containers/nav'
+
 export default {
   name: 'Home',
+  computed: {
+    navItems () {
+      return nav.items;
+    },
+  },
 }
 </script>
