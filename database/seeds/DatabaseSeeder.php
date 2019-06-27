80c56651042454346fea7a7fc0464ed558cc3115
mvune <mvune01@gmail.com>
2019-06-27 17:44:32 +0200

    Move dummy data out of DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UsersTableSeeder::class,
-            AccountsTableSeeder::class,
             BanksTableSeeder::class,
             TransactionFilterTargetsTableSeeder::class,
             TransactionTypesTableSeeder::class,
