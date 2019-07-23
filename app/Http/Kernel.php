e102704fb1834c1dadd1753f32e0cb27d5d3c558
mvune <mvune01@gmail.com>
2019-07-23 13:14:51 +0200

    Revert "Add CORS headers"
    This reverts commit 7153e1676cc4df2b655b893f6bb8aca29f5ec220.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,6 @@ class Kernel extends HttpKernel
 
         'api' => [
             'throttle:60,1',
-            'cors',
             'bindings',
         ],
     ];
@@ -58,7 +57,6 @@ class Kernel extends HttpKernel
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
-        'cors' => \App\Http\Middleware\CORS::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
